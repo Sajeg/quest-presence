3d3128de17ab6aba496286a5c04b8291ee23fa03
Sajeg <jfx@posteo.de>
2024-10-28 22:25:18 +0100

    Improved app close detection

--- app/src/main/java/com/sajeg/questrpc/classes/AccessibilityService.kt
+++ app/src/main/java/com/sajeg/questrpc/classes/AccessibilityService.kt
@@ -3,15 +3,14 @@ package com.sajeg.questrpc.classes
 import android.accessibilityservice.AccessibilityService
 import android.content.Intent
 import android.content.IntentFilter
+import android.util.Log
 import android.view.accessibility.AccessibilityEvent
 
 class AccessibilityService : AccessibilityService() {
 
     override fun onServiceConnected() {
         super.onServiceConnected()
-
         ActivityManager.start(this)
-
         val filter = IntentFilter().apply {
             addAction(Intent.ACTION_SCREEN_ON)
             addAction(Intent.ACTION_SCREEN_OFF)
@@ -29,6 +28,11 @@ class AccessibilityService : AccessibilityService() {
     override fun onAccessibilityEvent(p0: AccessibilityEvent?) {
         if (p0 != null && p0.eventType == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
             val packageName = p0.packageName?.toString()
+            Log.d("ActivityManagerPackage", packageName.toString())
+            Log.d("ActivityManagerClass", p0.className.toString())
+            if (packageName == "com.oculus.shellenv" && p0.className.toString() == "com.oculus.shellenv.ShellEnvActivity") {
+                ActivityManager.stop(this)
+            }
             ActivityManager.appChanged(packageName.toString(), this)
         }
     }
